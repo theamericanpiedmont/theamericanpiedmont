bcc97600f53f52c829b6d0e26d2b927c9b1d81e6
Rick Newkirk <ricknewkirk@mac.lan>
2026-03-05 06:44:12 -0500

    Add TAP essay module fixes

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,25 +1,45 @@
-import {type SchemaTypeDefinition} from 'sanity'
+import { type SchemaTypeDefinition } from "sanity"
 
-import {authorType} from './authorType'
-import {sectionType} from './sectionType'
-import {blockContentType} from './blockContentType'
-import {essayType} from './essayType'
-import {fieldNoteType} from './fieldNoteType'
-import {marginaliaType} from './marginaliaType'
-import {marginaliaSignalType} from './marginaliaSignalType'
-import {artifactType} from './artifactType'
+import { authorType } from "./authorType"
+import { sectionType } from "./sectionType"
+import { blockContentType } from "./blockContentType"
+import { essayType } from "./essayType"
+import { fieldNoteType } from "./fieldNoteType"
+import { marginaliaType } from "./marginaliaType"
+import { marginaliaSignalType } from "./marginaliaSignalType"
+import { artifactType } from "./artifactType"
 import { signalRunType } from "./signalRun"
 
-export const schema: {types: SchemaTypeDefinition[]} = {
-  types: [
-    authorType,
-    sectionType,
-    blockContentType,
-    essayType,
-    signalRunType,
-    fieldNoteType,
-    artifactType,
-    marginaliaType,
-    marginaliaSignalType,
-  ],
+import { pullQuoteType } from "./pullQuoteType"
+import { artifactEmbedType } from "./artifactEmbedType"
+import { storyImageType } from "./storyImageType"
+import { galleryType } from "./galleryType"
+
+const typePairs: Array<{ key: string; val: SchemaTypeDefinition | undefined }> = [
+  { key: "authorType", val: authorType },
+  { key: "sectionType", val: sectionType },
+  { key: "blockContentType", val: blockContentType },
+  { key: "essayType", val: essayType },
+  { key: "signalRunType", val: signalRunType },
+  { key: "fieldNoteType", val: fieldNoteType },
+  { key: "artifactType", val: artifactType },
+  { key: "marginaliaType", val: marginaliaType },
+  { key: "marginaliaSignalType", val: marginaliaSignalType },
+
+  // New ones
+  { key: "pullQuoteType", val: pullQuoteType },
+  { key: "artifactEmbedType", val: artifactEmbedType },
+  { key: "storyImageType", val: storyImageType },
+  { key: "galleryType", val: galleryType },
+]
+
+// Throw a readable error instead of Sanity’s generic SchemaError
+for (const t of typePairs) {
+  if (!t.val) throw new Error(`Sanity schema type import is undefined: ${t.key}`)
+  // @ts-expect-error Sanity types are structurally typed; name should exist
+  if (!t.val.name) throw new Error(`Sanity schema type missing "name": ${t.key}`)
+}
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: typePairs.map((t) => t.val!) as SchemaTypeDefinition[],
 }
